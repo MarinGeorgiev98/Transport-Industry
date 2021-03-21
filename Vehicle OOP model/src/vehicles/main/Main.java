23df1315c42ccae1e0b6bbb5196d5a605acd3837
Marin Georgiev <attention12@abv.bg>
2021-03-21 17:07:43 +0200

    Commit changes

--- Vehicle OOP model/src/vehicles/main/Main.java
+++ Vehicle OOP model/src/vehicles/main/Main.java
@@ -15,11 +15,12 @@ import vehicles.vehicle.VehicleColor;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         File fileData = new File("FileData.txt");
         Scanner reader = new Scanner(fileData);
 
