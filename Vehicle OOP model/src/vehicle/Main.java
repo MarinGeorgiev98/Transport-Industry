daf24fd88ce74ef3fb3848f8bbfb41d1bd4dac25
Marin Georgiev <attention12@abv.bg>
2021-03-21 17:21:28 +0200

    Commit changes

---
+++ Vehicle OOP model/src/vehicle/Main.java
@@ -0,0 +1,32 @@
+package vehicle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int fuel;
+        int horsePower;
+
+        fuel = Integer.parseInt(scan.nextLine());
+        horsePower = Integer.parseInt(scan.nextLine());
+        Vehicle raceMotorcycle = new RaceMotorcycle(fuel, horsePower);
+        System.out.println(raceMotorcycle);
+
+        fuel = Integer.parseInt(scan.nextLine());
+        horsePower = Integer.parseInt(scan.nextLine());
+        Vehicle crossMotorcycle = new CrossMotorcycle(fuel, horsePower);
+        System.out.println(crossMotorcycle);
+
+        fuel = Integer.parseInt(scan.nextLine());
+        horsePower = Integer.parseInt(scan.nextLine());
+        Vehicle familyCar = new FamilyCar(fuel, horsePower);
+        System.out.println(familyCar);
+
+        fuel = Integer.parseInt(scan.nextLine());
+        horsePower = Integer.parseInt(scan.nextLine());
+        Vehicle sportCar = new SportCar(fuel,horsePower);
+        System.out.println(sportCar);
+    }
+}
---
+++ Vehicle OOP model/src/vehicle/Vehicle.java
@@ -0,0 +1,58 @@
+package vehicle;
+
+public class Vehicle {
+    public static final double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    public double fuelConsumption;
+    double fuel;
+    int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public static double getDefaultFuelConsumption() {
+        return DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb
+                .append("Fuel consumption: ")
+                .append(this.getFuelConsumption())
+                .append(System.lineSeparator())
+                .append("Fuel: ")
+                .append(this.getFuel())
+                .append(System.lineSeparator())
+                .append("Horsepower: ")
+                .append(this.getHorsePower())
+                .append(System.lineSeparator());
+        return sb.toString();
+    }
+}
