375dd7fb43a0e7b761fbe8c98362d1f22f34a9ca
Marin Georgiev <attention12@abv.bg>
2021-03-24 05:21:20 +0200

    Commit changes

--- Vehicle OOP model/src/domainmodel/cars/FamilyCar.java
+++
@@ -1,9 +0,0 @@
-package domainmodel.cars;
-
-import domainmodel.enums.VehicleColor;
-
-public class FamilyCar extends Car {
-    public FamilyCar(double fuel, int horsePower, VehicleColor randomColor) {
-        super(fuel, horsePower, randomColor);
-    }
-}
